3d24dc30db2441626c9b37117cf193e0ab1437bb
Callan Davies <callan.davies1@gmail.com>
2018-07-30 12:01:47 +1000

    Slime now approaches player while avoiding obstacles. Movement is forwards and it rotates towards the player.

--- Syleria/Assets/Scripts/Enemy/Slime.cs
+++ Syleria/Assets/Scripts/Enemy/Slime.cs
@@ -45,9 +45,12 @@ public class Slime : Enemy
     // Update is called once per frame
     void Update()
     {
-        Vector3 v3right = transform.right;
-        transform.right = transform.up;
-        transform.up = v3right;
+        // Switch the objects right and up
+        // Temporary fix for testing purposes
+        // Will remove when it doesn't orbit its target when not switched.
+        //Vector3 v3right = transform.right;
+        //transform.right = transform.up;
+        //transform.up = v3right;
 
         Vector3 v3LastPos;
         if (!m_bCannotMove)
@@ -78,14 +81,32 @@ public class Slime : Enemy
     // Seek Towards the Player
     void Seek(Vector3 v3Target)
     {
-
-        Vector3 objectPos = transform.position;
-        v3Target.x = v3Target.x - objectPos.x;
-        v3Target.y = v3Target.y - objectPos.y;
-
-        float angle = Mathf.Atan2(v3Target.y, v3Target.x) * Mathf.Rad2Deg;
-        //Kinda works, however rotating instantly is not the wanted behaviour
-        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        // Find the target that will allow us to get to the target
+        Vector3 v3TargetDir = v3Target - transform.position;
+        v3TargetDir = v3TargetDir.normalized;
+        //Debug.Log(Vector3.Dot(transform.forward.normalized, v3TargetDir.normalized));
+        //Debug.Log("forward " + transform.forward);
+        //float angle = Mathf.Atan2(v3Target.y, v3Target.x) * Mathf.Rad2Deg;
+        //Kinda "works", however rotating instantly is not the wanted behaviour
+        //transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        Debug.DrawRay(this.transform.position, v3TargetDir, Color.red);
+        float dot = Vector3.Dot(transform.up.normalized, v3TargetDir);
+        float rightDot = Vector3.Dot(transform.right.normalized, v3TargetDir);
+        if (dot > 0.9f)
+        {
+            // Don't rotate if facing target
+            Debug.Log("AAAA");
+        }
+        else if (rightDot > 0) // rotate right as D is on the right.
+        {
+            transform.Rotate(Vector3.back * (90f * Time.deltaTime));
+            Debug.Log("Right");
+        }
+        else if(rightDot < 0) // rotate left as D is on the Left
+        {
+            transform.Rotate(Vector3.forward * (90f * Time.deltaTime));
+            Debug.Log("Left");
+        }
 
         // Rotates on spot very fast, never seeks
         //Vector3.RotateTowards(transform.position, new Vector3(0, 0, angle),5f,5f);
@@ -94,7 +115,11 @@ public class Slime : Enemy
         //Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 0, angle), 2f * Time.deltaTime);
 
         // Goes diagonally downwards whilst rotating back and forth
-       // transform.Rotate(new Vector3(0, 0, angle * Time.deltaTime * 0.00001f));
+        //transform.Rotate(new Vector3(0, 0, angle));
+
+        // Goes diagonally downards whilst rotating around all angles
+        //Quaternion newRotation = Quaternion.AngleAxis(angle, Vector3.up);
+        //transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, .05f);
 
     }
 
@@ -105,9 +130,9 @@ public class Slime : Enemy
 
         int count = 0;
 
-        count = Physics2D.Raycast((Vector2)transform.position, m_v2Velocity, m_cfFilter, aHit, 5.0f);
-        
+        count = Physics2D.Raycast((Vector2)transform.position, transform.up, m_cfFilter, aHit, 5.0f);
 
+        Debug.DrawRay(transform.position, transform.up, Color.red);
         if (count > 0)
         {
             m_bMiddleHit = true;
@@ -118,7 +143,7 @@ public class Slime : Enemy
         }
 
         Vector2 leftR = (Vector2)transform.position;
-        count = Physics2D.Raycast((Vector2)m_gLeftFeeler.transform.position, m_v2Velocity, m_cfFilter, aHit, 5.0f);
+        count = Physics2D.Raycast((Vector2)m_gLeftFeeler.transform.position, transform.up, m_cfFilter, aHit, 5.0f);
 
         if (count > 0)
         {
@@ -130,7 +155,7 @@ public class Slime : Enemy
         }
 
         Vector2 rightR = (Vector2)transform.position;
-        count = Physics2D.Raycast((Vector2)m_gRightFeeler.transform.position,m_v2Velocity, m_cfFilter, aHit, 5.0f);
+        count = Physics2D.Raycast((Vector2)m_gRightFeeler.transform.position, transform.up, m_cfFilter, aHit, 5.0f);
 
         if (count > 0)
         {
