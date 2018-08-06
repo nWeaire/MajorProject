9415fbdcdc7cdd128b158db7c6cc309a107465f0
Callan Davies <callan.davies1@gmail.com>
2018-08-06 14:02:28 +1000

    Added EnemyBullet.cs, EnemyBullet prefab and SnapToGrid.cs

---
+++ Syleria/Assets/Scripts/Bullet/EnemyBullet.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    public float m_fRange;
+
+    private GameObject m_gPlayer;
+
+    private Vector2 m_v2StartPos;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_gPlayer = GameObject.FindGameObjectWithTag("Player");
+        m_v2StartPos = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckRange();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            Destroy(this.gameObject);
+        }
+        if(collision.tag == "Player")
+        {
+            m_gPlayer.GetComponent<Player>().AddCurrentHealth(-10);
+
+
+            Debug.Log("Enemy Bullet Hit");
+            Destroy(this.gameObject);
+        }
+    }
+
+    void CheckRange()
+    {
+        if (Vector2.Distance(this.transform.position, m_v2StartPos) >= m_fRange)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Syleria/Assets/Scripts/Camera/SnapToGrid.cs
@@ -0,0 +1,18 @@
+﻿// Script made by GitHub User "Misato"
+// https://gist.github.com/misato/03a89a32b2424754cc07533040dd236f#file-snaptogrid-cs
+using UnityEngine;
+
+public class SnapToGrid : MonoBehaviour
+{
+    public float PPU = 8.4375f; // pixels per unit (your tile size)
+
+    private void LateUpdate()
+    {
+        Vector3 position = transform.localPosition;
+
+        position.x = (Mathf.Round(transform.parent.position.x * PPU) / PPU) - transform.parent.position.x;
+        position.y = (Mathf.Round(transform.parent.position.y * PPU) / PPU) - transform.parent.position.y;
+
+        transform.localPosition = position;
+    }
+}
