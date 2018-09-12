003adb6827ce9c7b3b7c8739f8db494fd2ac5edc
nWeaire <n.weaire@gmail.com>
2018-09-12 16:52:05 +1000

    World generation implemented. RoomPrefabs now have Corridors.cs and floor moved to prefab

--- Syleria/Assets/Scripts/World Gen/Room.cs
+++ Syleria/Assets/Scripts/World Gen/Room.cs
@@ -2,82 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Room : MonoBehaviour {
+public class Room : MonoBehaviour
+{
 
-    public GameObject northDoor;
-    public GameObject southDoor;
-    public GameObject eastDoor;
-    public GameObject westDoor;
+    public int indexX;
+    public int indexY;
+    public Vector2 worldPosition;
+    public bool isBoss = false;
+    public bool isShop = false;
+    public bool isRoom = false;
+    public bool isSpawn = false;
 
-    public GameObject northCorridor;
-    public GameObject southCorridor;
-    public GameObject eastCorridor;
-    public GameObject westCorridor;
+    public GameObject room;
 
-    public GameObject centre;
-    public GameObject shop;
-    public GameObject boss;
-    
-    public bool openNorth;
-    public bool openSouth;
-    public bool openEast;
-    public bool openWest;
-
-    public bool centreOn;
-    public bool shopOn;
-    public bool bossOn;
-
-
-    private void Start()
+    public Room(int indX, int indY, Vector2 worldpos)
     {
-        //openNorth = false;
-        //openSouth = false;
-        //openEast = false;
-        //openWest = false;
-
-        //centreOn = false;
-        //shopOn = false;
-        //bossOn = false;
+        indexX = indX;
+        indexY = indY;
+        worldPosition = worldpos;
     }
-
-    private void Update()
-    {
-        if (centreOn)
-        {
-            centre.SetActive(true);
-        }
-        if (shopOn)
-        {
-            shop.SetActive(true);
-        }
-        if (bossOn)
-        {
-            boss.SetActive(true);
-        }
-
-        if (openNorth)
-        {
-            northDoor.SetActive(false);
-            northCorridor.SetActive(true);
-        }
-        if (openSouth)
-        {
-            southDoor.SetActive(false);
-            southCorridor.SetActive(true);
-        }
-        if (openEast)
-        {
-            eastDoor.SetActive(false);
-            eastCorridor.SetActive(true);
-        }
-        if (openWest)
-        {
-            westDoor.SetActive(false);
-            westCorridor.SetActive(true);
-        }
-
-    }
-
-
 
 }
--- Syleria/Assets/Scripts/World Gen/WorldGeneration.cs
+++ Syleria/Assets/Scripts/World Gen/WorldGeneration.cs
@@ -2,47 +2,154 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WorldGeneration : MonoBehaviour {
+public class WorldGeneration : MonoBehaviour
+{
 
-    public class Room
-    {
-        int indexX;
-        int indexY;
-        Vector2 worldPosition;
-        bool isBoss;
-        bool isShop;
-
-        public Room(int indX, int indY, Vector2 worldpos)
-        {
-            indexX = indX;
-            indexY = indY;
-            worldPosition = worldpos;
-        }
-
-    };
-
-    private int m_nArrayHeight;
-    private int m_nArrayWidth;
-    private Room[,] m_aRoomArray;
+    [SerializeField] private int m_nArrayHeight;
+    [SerializeField] private int m_nArrayWidth;
+    public Room[,] m_aRoomArray;
+    private Vector2Int m_v2StartRoom;
 
     [SerializeField] private int m_nRoomCount;
+    public int m_nRoomsSpawned;
     [SerializeField] private GameObject m_gRoom;
     [SerializeField] private GameObject m_gStartRoom;
-    [SerializeField] private Vector2 m_nRoomSize;
+    [SerializeField] private Vector2 m_v2RoomSize;
+    public Vector2Int m_v2CurrentIndex;
+    private bool m_bCorridors = false;
+    private bool m_bAreRoomsSpawned = false;
 
-
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
-        
+        CreateGrid();
 
-        Instantiate(m_gStartRoom, new Vector2(0, 0), new Quaternion(), this.transform);
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    private void CreateGrid()
     {
+        m_aRoomArray = new Room[m_nArrayWidth, m_nArrayHeight];
+        for (int i = 0; i < m_nArrayWidth; i++)
+        {
+            for (int j = 0; j < m_nArrayHeight; j++)
+            {
+                m_aRoomArray[i, j] = new Room(i, j , new Vector2(i * m_v2RoomSize.x, j * m_v2RoomSize.y));
+            }
+        }
+        m_v2StartRoom.x = (m_nArrayWidth - 1) / 2;
+        m_v2StartRoom.y = (m_nArrayHeight - 1) / 2;
+        m_v2CurrentIndex = m_v2StartRoom;
+        m_aRoomArray[m_v2StartRoom.x, m_v2StartRoom.y].isSpawn = true;
+        m_aRoomArray[m_v2CurrentIndex.x, m_v2CurrentIndex.y].room = Instantiate(m_gStartRoom, m_aRoomArray[m_v2StartRoom.x, m_v2StartRoom.y].worldPosition, new Quaternion(), this.transform);
+    }
 
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_nRoomsSpawned < m_nRoomCount - 1)
+        {
+            SpawnRooms();
+        }
+        if (!m_bCorridors && m_bAreRoomsSpawned)
+        {
+            for (int i = 0; i < m_nArrayWidth; i++)
+            {
+                for (int j = 0; j < m_nArrayHeight; j++)
+                {
+                    if (m_aRoomArray[i, j].isRoom)
+                    {
+                        
+                    }
+                    else
+                    {
 
+                    }
+                }
+            }
+            m_bCorridors = true;
+        }
+    }
+
+    private void SpawnRooms()
+    {
+        // 0 = center, 1 = up, 2 = down, 3 = left, 4 = right
+        int dir = Random.Range(0, 4);
+        switch (dir)
+        {
+            case 0:
+                m_v2CurrentIndex = m_v2StartRoom;
+                break;
+            case 1:
+                m_v2CurrentIndex = new Vector2Int(m_v2CurrentIndex.x, m_v2CurrentIndex.y + 1);
+                if (m_aRoomArray[m_v2CurrentIndex.x, m_v2CurrentIndex.y].isRoom != true)
+                {
+                    m_aRoomArray[m_v2CurrentIndex.x, m_v2CurrentIndex.y].isRoom = true;
+                    m_aRoomArray[m_v2CurrentIndex.x, m_v2CurrentIndex.y].room = Instantiate(m_gRoom, m_aRoomArray[m_v2CurrentIndex.x, m_v2CurrentIndex.y].worldPosition, new Quaternion(), this.transform);
+                    m_aRoomArray[m_v2CurrentIndex.x, m_v2CurrentIndex.y - 1].room.GetComponent<Corridors>().topCorridor.SetActive(true);
+                    m_aRoomArray[m_v2CurrentIndex.x, m_v2CurrentIndex.y].room.GetComponent<Corridors>().bottomCorridor.SetActive(true);
+                    m_aRoomArray[m_v2CurrentIndex.x, m_v2CurrentIndex.y - 1].room.GetComponent<Corridors>().topWall.SetActive(false);
+                    m_aRoomArray[m_v2CurrentIndex.x, m_v2CurrentIndex.y].room.GetComponent<Corridors>().bottomWall.SetActive(false);
+                    m_nRoomsSpawned += 1;
+                }
+                else
+                {
+
+                }
+                break;
+            case 2:
+                m_v2CurrentIndex = new Vector2Int(m_v2CurrentIndex.x, m_v2CurrentIndex.y - 1);
+                if (m_aRoomArray[m_v2CurrentIndex.x, m_v2CurrentIndex.y].isRoom != true)
+                {
+                    m_aRoomArray[m_v2CurrentIndex.x, m_v2CurrentIndex.y].isRoom = true;
+                    m_aRoomArray[m_v2CurrentIndex.x, m_v2CurrentIndex.y].room = Instantiate(m_gRoom, m_aRoomArray[m_v2CurrentIndex.x, m_v2CurrentIndex.y].worldPosition, new Quaternion(), this.transform);
+                    m_aRoomArray[m_v2CurrentIndex.x, m_v2CurrentIndex.y + 1].room.GetComponent<Corridors>().bottomCorridor.SetActive(true);
+                    m_aRoomArray[m_v2CurrentIndex.x, m_v2CurrentIndex.y + 1].room.GetComponent<Corridors>().bottomWall.SetActive(false);
+                    m_aRoomArray[m_v2CurrentIndex.x, m_v2CurrentIndex.y].room.GetComponent<Corridors>().topCorridor.SetActive(true);
+                    m_aRoomArray[m_v2CurrentIndex.x, m_v2CurrentIndex.y].room.GetComponent<Corridors>().topWall.SetActive(false);
+                    m_nRoomsSpawned += 1;
+                }
+                else
+                {
+
+                }
+                break;
+            case 3:
+                m_v2CurrentIndex = new Vector2Int(m_v2CurrentIndex.x - 1, m_v2CurrentIndex.y);
+                if (m_aRoomArray[m_v2CurrentIndex.x, m_v2CurrentIndex.y].isRoom != true)
+                {
+                    m_aRoomArray[m_v2CurrentIndex.x, m_v2CurrentIndex.y].isRoom = true;
+                    m_aRoomArray[m_v2CurrentIndex.x, m_v2CurrentIndex.y].room = Instantiate(m_gRoom, m_aRoomArray[m_v2CurrentIndex.x, m_v2CurrentIndex.y].worldPosition, new Quaternion(), this.transform);
+                    m_aRoomArray[m_v2CurrentIndex.x + 1, m_v2CurrentIndex.y].room.GetComponent<Corridors>().leftCorridor.SetActive(true);
+                    m_aRoomArray[m_v2CurrentIndex.x, m_v2CurrentIndex.y].room.GetComponent<Corridors>().rightCorridor.SetActive(true);
+                    m_aRoomArray[m_v2CurrentIndex.x + 1, m_v2CurrentIndex.y].room.GetComponent<Corridors>().leftWall.SetActive(false);
+                    m_aRoomArray[m_v2CurrentIndex.x, m_v2CurrentIndex.y].room.GetComponent<Corridors>().rightWall.SetActive(false);
+                    m_nRoomsSpawned += 1;
+                }
+                else
+                {
+
+                }
+                break;
+            case 4:
+                m_v2CurrentIndex = new Vector2Int(m_v2CurrentIndex.x + 1, m_v2CurrentIndex.y);
+                if (m_aRoomArray[m_v2CurrentIndex.x, m_v2CurrentIndex.y].isRoom != true)
+                {
+                    m_aRoomArray[m_v2CurrentIndex.x, m_v2CurrentIndex.y].isRoom = true;
+                    m_aRoomArray[m_v2CurrentIndex.x, m_v2CurrentIndex.y].room = Instantiate(m_gRoom, m_aRoomArray[m_v2CurrentIndex.x, m_v2CurrentIndex.y].worldPosition, new Quaternion(), this.transform);
+                    m_aRoomArray[m_v2CurrentIndex.x - 1, m_v2CurrentIndex.y].room.GetComponent<Corridors>().rightCorridor.SetActive(true);
+                    m_aRoomArray[m_v2CurrentIndex.x, m_v2CurrentIndex.y].room.GetComponent<Corridors>().leftCorridor.SetActive(true);
+                    m_aRoomArray[m_v2CurrentIndex.x - 1, m_v2CurrentIndex.y].room.GetComponent<Corridors>().rightWall.SetActive(false);
+                    m_aRoomArray[m_v2CurrentIndex.x, m_v2CurrentIndex.y].room.GetComponent<Corridors>().leftWall.SetActive(false);
+                    m_nRoomsSpawned += 1;
+                }
+                else
+                {
+
+                }
+                break;
+            default:
+                break;
+        }
+        m_bAreRoomsSpawned = true;
+    }
 }
