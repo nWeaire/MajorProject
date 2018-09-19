1e94337e0aef065b2bc2ed629347d48810557621
Callan Davies <callan.davies1@gmail.com>
2018-09-19 11:58:32 +1000

    Fixed: - Sentry code commented and cleaned. - Sentry won't flip the eye any more.

--- Syleria/Assets/Scripts/Enemy/Sentry.cs
+++ Syleria/Assets/Scripts/Enemy/Sentry.cs
@@ -13,44 +13,48 @@ using UnityEngine;
 
 public class Sentry : Enemy
 {
-    // Projectile for the Sentry
+    // Projectile for the Sentry.
     [Tooltip("Put Enemy Bullet prefab here")]
     public GameObject m_gProjectile;
 
-    // Speed that the bullet will go at
+    // Speed that the bullet will go at.
     [Tooltip("Speed that the bullet will be fired at")]
     public float m_fBulletSpeed;
 
-    // Timer between shots
+    // Timer between shots.
     [Tooltip("Seconds between shots")]
     public float m_fFireRate;
 
-    // Time between bursts
+    // Time between bursts.
     [Tooltip("Seconds between bursts")]
     public float m_fBurstTimer;
 
-    // Amount of shots per burst
+    // Amount of shots per burst.
     [Tooltip("Amount of shots per burst")]
     public int m_nBurstAmount;
 
+    // Pointer to the eye of the Sentry.
     public GameObject m_gEye;
 
     [Tooltip("Time in Seconds that the enemy will sit still after spawn")]
     public float m_fSpawnTime;
 
-    // a timer for use in timing the shots
+    // a timer for use in timing the shots.
     private float m_fTimeBetweenShots = 0;
 
-    // a timer for use in timing between bursts
+    // a timer for use in timing between bursts.
     private float m_fTimeBetweenBursts;
 
-    // a counter for the shots in a burst
+    // a counter for the shots in a burst.
     private int m_nBurstCount;
 
+    // Vector for what the Sentry is shooting at.
     private Vector3 m_v3Target;
-
+    
+    // Timer for the Sentry not acting for a period of time after spawning.
     private float m_fSpawnTimer = 0.0f;
 
+    // boolean for the Sentry not acting for a period of time after spawning.
     private bool m_bSpawnStun;
 
     //--------------------------------------------------------------------------------------
@@ -59,10 +63,8 @@ public class Sentry : Enemy
     new void Awake()
     {
         base.Awake();
-        // Get Player.
         m_bSpawnStun = true;
         m_gPlayer = GameObject.FindGameObjectWithTag("Player");
-        // Set the counter to max timer.
         m_fTimeBetweenShots = m_fFireRate;
     }
 
@@ -71,22 +73,13 @@ public class Sentry : Enemy
     //--------------------------------------------------------------------------------------
     new void Update()
     {
+        // If the sentry has passed the spawn timer.
         if (!m_bSpawnStun)
         {
             base.Update();
-            // Boolean setting for the sprite
-            if (transform.position.x - m_gPlayer.transform.position.x >= 0)
-            {
-                // Face left 
-                m_gEye.GetComponent<SpriteRenderer>().flipX = false;
-            }
-            else
-            {
-                // Face right
-                m_gEye.GetComponent<SpriteRenderer>().flipX = true;
-            }
-            // increase timer
+            // increase timer.
             m_fTimeBetweenShots += Time.deltaTime;
+            // make it so it can be related to seconds.
             m_fTimeBetweenShots = m_fTimeBetweenShots % 60;
 
             // if timer has reached the limit,
@@ -95,16 +88,21 @@ public class Sentry : Enemy
                 // if burst amount is less than the amount of shots wanted,
                 if (m_nBurstCount < m_nBurstAmount)
                 {
+                    // If the sentry isn't taunted
                     if (!m_bTaunted)
                     {
+                        // If the Sentry can see the player.
                         if (!Physics2D.Linecast((Vector2)this.transform.position, (Vector2)m_gPlayer.transform.position, m_WallLayer))
                         {
                             Fire();
                         }
                     }
+                    // If the sentry is taunted
                     else
                     {
+                        // Find the taunting turtle
                         m_gCompanion = GameObject.FindGameObjectWithTag("Turtle");
+                        // If the Sentry can see the turtle.
                         if (!Physics2D.Linecast((Vector2)this.transform.position, (Vector2)m_gCompanion.transform.position, m_WallLayer))
                         {
                             Fire();
@@ -134,6 +132,7 @@ public class Sentry : Enemy
                 Die();
             }
         }
+        // If the Sentry has not left spawn stun.
         else
         {
             // Increment timer.
@@ -160,29 +159,31 @@ public class Sentry : Enemy
         GameObject newBullet = Instantiate(m_gProjectile, this.transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;
         m_nBurstCount++;
 
+        // If sentry istaunted
         if (m_bTaunted)
         {
-            
+            // Aim at the turtle.
             m_v3Target = m_gCompanion.transform.position - transform.position;
             m_v3Target.Normalize();
         }
         else
         {
-            // Get the target position
+            // Aim at the Player.
             m_v3Target = m_gPlayer.transform.position - transform.position;
             m_v3Target.Normalize();
         }
-        // Calculate rotation needed to face Player
+
+        // Calculate rotation needed to face the target.
         float angle = Mathf.Atan2(m_v3Target.y, m_v3Target.x) * Mathf.Rad2Deg;
         // Set bullets rotation to face Player.
         newBullet.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
         
-        // Set bullets damage to this Sentrys damage value
+        // Set bullets damage to this Sentrys damage value.
         newBullet.GetComponent<EnemyBullet>().m_nDam = m_nDamage;
-        // Set bullets damage to this Sentrys bullet speed
+        // Set bullets damage to this Sentrys bullet speed.
         newBullet.GetComponent<EnemyBullet>().m_fSpeed = m_fBulletSpeed;
 
-        // Reset timer
+        // Reset shots timer.
         m_fTimeBetweenShots = 0.0f;
     }
 
