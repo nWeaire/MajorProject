7aa435e85299509ec7a7e69ce14418602f4787e9
Callan Davies <callan.davies1@gmail.com>
2018-10-16 14:04:00 +1100

    Changed: - Commented out the Slash projectile spawning.

--- Syleria/Assets/Scripts/Companion/Slash.cs
+++ Syleria/Assets/Scripts/Companion/Slash.cs
@@ -31,6 +31,7 @@ public class Slash : MonoBehaviour
     private bool m_bSpawnedSlash = false;
 
     private GameObject gSlash;
+
     // Use this for initialization
     void Awake()
     {
@@ -43,19 +44,19 @@ public class Slash : MonoBehaviour
     {
         if (!m_bSpawnedSlash)
         {
-            gSlash = Instantiate(m_gSlash, m_gPlayer.transform);
-            gSlash.transform.position = m_gAim.transform.position;
-            m_bSpawnedSlash = true;
+            //gSlash = Instantiate(m_gSlash, m_gPlayer.transform);
+            //gSlash.transform.position = m_gAim.transform.position;
+            //m_bSpawnedSlash = true;
         }
-        gSlash.transform.RotateAround(m_gPlayer.transform.position, new Vector3(0, 0, -1), m_fSlashSpeed * Time.deltaTime);
-        Vector3 desiredPosition = (gSlash.transform.position - m_gPlayer.transform.position).normalized * 4 + m_gPlayer.transform.position;
-        gSlash.transform.position = Vector3.MoveTowards(gSlash.transform.position, desiredPosition, Time.deltaTime * m_fSlashSpeed);
+        //gSlash.transform.RotateAround(m_gPlayer.transform.position, new Vector3(0, 0, -1), m_fSlashSpeed * Time.deltaTime);
+        //Vector3 desiredPosition = (gSlash.transform.position - m_gPlayer.transform.position).normalized * -3 + m_gPlayer.transform.position;
+        //gSlash.transform.position = Vector3.MoveTowards(gSlash.transform.position, desiredPosition, Time.deltaTime * m_fSlashSpeed);
         m_fSlashTimer += Time.deltaTime; // Updates timer 
         m_fSlashTimer = m_fSlashTimer % 60;
         if (m_fSlashTimer >= m_fSlashTime) // If timer > sprite length
         {
             m_bSpawnedSlash = false;
-            GameObject.Destroy(gSlash);
+            //GameObject.Destroy(gSlash);
             this.gameObject.SetActive(false); // Set this to false
             m_fSlashTimer = 0.0f;
         }
