82aacd3004162b5ff03ff465f333bb423c113e3d
nWeaire <n.weaire@gmail.com>
2018-11-05 13:38:51 +1100

    Added  - Script for Destructables in the scene

---
+++ Syleria/Assets/Scripts/Items/Destructables.cs
@@ -0,0 +1,28 @@
+﻿//--------------------------------------------------------------------------------------
+// Purpose: Destructable objects in scene logic
+//
+// Author: Nicholas Weaire
+//--------------------------------------------------------------------------------------
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructables : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        { }
+        else
+        {
+            Destroy(this.gameObject); // Destroys this game object
+        }
+    }
+
+    private void OnDestroy()
+    {
+        // Animation to play here
+        Debug.Log("Destroyed");
+    }
+}
