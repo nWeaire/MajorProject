c493233ea8f53db0bd835ad777a7b3d005aaf52a
Callan Davies <callan.davies1@gmail.com>
2018-11-07 12:25:34 +1100

    Changed: - Cleaned up Slash.cs with commenting and removal of unneeded functions and variables.

--- Syleria/Assets/Scripts/Companion/Slash.cs
+++ Syleria/Assets/Scripts/Companion/Slash.cs
@@ -11,52 +11,25 @@ using UnityEngine;
 
 public class Slash : MonoBehaviour
 {
-    // Pointer to Player.
-    public GameObject m_gPlayer;
-    // Pointer to the direction the slash should start at.
-    public GameObject m_gAim;
-
     // Time until the Slash is deleted
     public float m_fSlashTime = 0.1f;
 
-    // Speed that the Slash will move at
-    public float m_fSlashSpeed = 400f;
-
-    public GameObject m_gSlash;
-
+    // Timer for the Slash.
     private float m_fSlashTimer = 0;
 
-    private Animator m_Animator;
 
-    private bool m_bSpawnedSlash = false;
-
-    private GameObject gSlash;
-
-    // Use this for initialization
-    void Awake()
-    {
-        m_fSlashTimer = 0; // Timer set to 0
-        m_Animator = GetComponent<Animator>();
-    }
-
-    // Update is called once per frame
+    //--------------------------------------------------------------------------------------
+    // Update: Function that calls each frame to update game objects.
+    //--------------------------------------------------------------------------------------
     void Update()
     {
-        if (!m_bSpawnedSlash)
-        {
-            //gSlash = Instantiate(m_gSlash, m_gPlayer.transform);
-            //gSlash.transform.position = m_gAim.transform.position;
-            //m_bSpawnedSlash = true;
-        }
-        //gSlash.transform.RotateAround(m_gPlayer.transform.position, new Vector3(0, 0, -1), m_fSlashSpeed * Time.deltaTime);
-        //Vector3 desiredPosition = (gSlash.transform.position - m_gPlayer.transform.position).normalized * -3 + m_gPlayer.transform.position;
-        //gSlash.transform.position = Vector3.MoveTowards(gSlash.transform.position, desiredPosition, Time.deltaTime * m_fSlashSpeed);
-        m_fSlashTimer += Time.deltaTime; // Updates timer 
+        // Update timer 
+        m_fSlashTimer += Time.deltaTime; 
         m_fSlashTimer = m_fSlashTimer % 60;
-        if (m_fSlashTimer >= m_fSlashTime) // If timer > sprite length
+
+        // If timer > sprite length
+        if (m_fSlashTimer >= m_fSlashTime) 
         {
-            m_bSpawnedSlash = false;
-            //GameObject.Destroy(gSlash);
             this.gameObject.SetActive(false); // Set this to false
             m_fSlashTimer = 0.0f;
         }
