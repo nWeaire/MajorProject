8a0cc7ad84bb4303a6c76e51bf3d73688c9fc39c
Callan Davies <callan.davies1@gmail.com>
2018-11-14 10:58:18 +1100

    Added: - TriggerObject.cs that activates a public gameobject.

---
+++ Syleria/Assets/Scripts/Room interaction/TriggerObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerObject : MonoBehaviour
+{
+    public GameObject m_gObject;
+	// Use this for initialization
+	void Awake()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            m_gObject.SetActive(true);
+        }
+    }
+}
