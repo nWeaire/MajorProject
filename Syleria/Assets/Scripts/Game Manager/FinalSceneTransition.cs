db4980212198f320cd704443513f5a82a417a1b3
nWeaire <n.weaire@gmail.com>
2018-11-13 11:37:50 +1100

    Changed  - FinalSceneTransition now sets companions and players local position to 0

--- Syleria/Assets/Scripts/Game Manager/FinalSceneTransition.cs
+++ Syleria/Assets/Scripts/Game Manager/FinalSceneTransition.cs
@@ -30,6 +30,8 @@ public class FinalSceneTransition : MonoBehaviour {
             GameObject.FindObjectOfType<Light>().intensity = m_intensityForGauntletRoom;
             m_gRunTimeObjects = GameObject.FindGameObjectWithTag("SceneObjects"); // Finds all runtime objects
             m_gRunTimeObjects.transform.position = m_gFinalRoom.transform.position; // Sets runtime objects position to the final rooms
+            GameObject.Find("Player").transform.localPosition = Vector2.zero; // Finds and sets local position of player to zero
+            GameObject.FindGameObjectWithTag("Companion").transform.localPosition = Vector2.zero; // finds and sets companions local position to zero
             m_gAStar = GameObject.FindGameObjectWithTag("A*"); // find a* object
             m_gAStar.transform.position = m_gFinalRoom.transform.position + new Vector3(-100, -100, 0); // Sets a star to bottom left of the final rooms position
             m_gAStar.GetComponent<aStarGrid>().CreateGrid(200, 200); // Creates new a* grid
