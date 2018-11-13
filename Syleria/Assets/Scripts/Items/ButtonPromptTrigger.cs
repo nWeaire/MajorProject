cd428a51ac88f6a367cbd87096d456a34bbb8a3a
nWeaire <n.weaire@gmail.com>
2018-11-13 13:45:56 +1100

    Added  - ButtonPromptTrigger.cs

---
+++ Syleria/Assets/Scripts/Items/ButtonPromptTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ButtonPromptTrigger : MonoBehaviour {
+
+    private GameObject m_gButtonPrompt;
+
+    private void Awake()
+    {
+        m_gButtonPrompt = GameObject.FindGameObjectWithTag("ButtonPrompt");
+        m_gButtonPrompt.GetComponent<Image>().enabled = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            m_gButtonPrompt.GetComponent<Image>().enabled = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            m_gButtonPrompt.GetComponent<Image>().enabled = false;
+        }
+    }
+}
